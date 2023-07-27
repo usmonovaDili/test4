690e2f995db7808d5dbecbcba4337662e6a8581e
usmonovaDili <diloromusmonova202@gmail.com>
2023-07-27 08:38:11 +0500

    django4

---
+++ add4/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def book(request):
+    return  HttpResponse('jfvijrjnvjnvn34uivijrvjnkfcnr')
+
+def book2(request):
+    return HttpResponse('diuefuihfhbjhgqydjhbfhh')
